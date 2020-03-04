a5fa69d9e34b4414df095aee6fcf425394fa7948
gildej30 <54160024+gildej30@users.noreply.github.com>
2020-03-04 16:55:07 -0600

    end of day updates

--- prop-it-up/src/components/PersonalCard.js
+++ prop-it-up/src/components/PersonalCard.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 
+const Button = (props) => {
+    return (
+        <button onClick={props.age}>Birthday</button>
+    )
+}
+
 class PersonalCard extends Component {
     constructor(props){
         super(props);
+        this.state = {
+            age : 1
+        }
     }
+
+    birthday = () => {
+        this.setState({
+            age: this.state.age += 1
+
+        })
+    }
+
     render() {
-        const {firstName, lastName,age,hairColor} = this.props;
+        const {firstName, lastName, age, hairColor} = this.props;
         return( 
             <div>
                 <h2>{firstName} {lastName}</h2>
-                <h3>Age:{age}</h3>
                 <h3>Hair Color:{hairColor}</h3>
+                <h3>Age:{age}</h3>
+                <Button birthday={this.age} age={this.state.age}/>
             </div>
         );
     }
